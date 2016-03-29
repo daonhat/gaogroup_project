0185220c2d449006cfe7cc9f34cac8eac6e14705
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-03-29 07:30:01 +0000

    update delete post and alert

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :edit, :update, :destroy]  
     def index
         @posts = Post.all
     end
     
     def create
-        @post = Post.create(post_params)
-        redirect_to posts_path
+        if @post = Post.create(post_params)
+            flash[:success] = "Your post has been created!"
+            redirect_to posts_path
+        else
+            flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+            render :new
+        end
     end
     
     def new
@@ -22,12 +28,30 @@ class PostsController < ApplicationController
     
     def update
         @post = Post.find(params[:id])
-        @post.update(post_params)
+        
+     if @post.update(post_params)
+        flash[:success] = "Post updated." 
         redirect_to(post_path(@post))
+     else
+        flash.now[:alert] = "Update failed.  Please check the form."
+        render :edit
+     end
     end
-    
+    def destroy
+       @post = Post.find(params[:id])
+     if @post.destroy
+        flash[:success] = "Post have been deleted"
+        redirect_to posts_path
+     else
+        flash.now[:alert] = "Delete failed"
+        render :edit
+     end
+    end
 private
     def post_params
         params.require(:post).permit(:image, :caption)
+    end
+    def set_post
+        @post = Post.find(params[:id])
     end
 end
